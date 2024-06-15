6c678c8bac82f569a39d42764c41ec4e7e9b8764
Kevin Morales <kfmorales94@gmail.com>
2024-06-15 10:49:36 -0500

    Project done

--- app/src/main/java/com/kevinhomorales/sharedpreferencekotlin/MainActivity.kt
+++ app/src/main/java/com/kevinhomorales/sharedpreferencekotlin/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kevinhomorales.sharedpreferencekotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpView()
+    }
+
+    private fun setUpView() {
+        val NOT_DATA = getString(R.string.not_data_text)
+        val sharedPreferences = getSharedPreferences("user_preferences", MODE_PRIVATE)
+        val data = sharedPreferences.getString("user_data", NOT_DATA)
+        binding.textId.text = if (data != null && data.isNotEmpty()) data else NOT_DATA
+        setUpActions()
+    }
+
+    private fun setUpActions() {
+        binding.buttonId.setOnClickListener {
+            saveData()
         }
+    }
+
+    private fun saveData() {
+        val sharedPreferences = getSharedPreferences("user_preferences", MODE_PRIVATE)
+        val data = binding.editTextId.text.toString()
+        with(sharedPreferences.edit()) {
+            putString("user_data", data)
+            apply()
+        }
+        binding.textId.setText(data)
     }
 }
